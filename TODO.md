e80a719706e9c398493832a28fe3cfc0834c30ea
KTrain5369 <me@git.ktrain5369.dev>
2026-08-12 15:44:58 +1000

    blank files for most other things

---
+++ TODO.md
@@ -0,0 +1,10 @@
+# To-do list
+
+## Provider implementation list
+
+- [ ] Cloudflare (R2 as Binding & HTTP)
+- [ ] Vercel (Blobs)
+- [ ] Netlify (Blobs)
+- [ ] AWS (S3)
+- [ ] Node (fs)
+- [ ] Memory (memfs)
--- src/drivers/cloudflare.ts
+++ src/drivers/cloudflare.ts
@@ -1,3 +1,5 @@
 import type { R2Bucket } from "@cloudflare/workers-types";
 
 export function R2Binding<const TRawBucket extends R2Bucket>(bucket: TRawBucket, options?: {}) {}
+
+export function R2HTTP(options?: {}) {}
