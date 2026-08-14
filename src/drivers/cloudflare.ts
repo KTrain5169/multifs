fbcb92f0c4845a1962f0f9ed3e611597949dbf6a
KTrain5369 <me@git.ktrain5369.dev>
2026-08-14 21:41:05 +1000

    add cloudflare sdk (nothing for now)

--- src/drivers/cloudflare.ts
+++ src/drivers/cloudflare.ts
@@ -2,4 +2,10 @@ import type { R2Bucket } from "@cloudflare/workers-types";
 
 export function R2Binding<const TRawBucket extends R2Bucket>(bucket: TRawBucket, options?: {}) {}
 
-export function R2HTTP(options?: {}) {}
+export function R2HTTP(
+  bucket: string,
+  options?: {
+    accountId: string;
+    apiKey: string;
+  },
+) {}
